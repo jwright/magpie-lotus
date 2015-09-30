21499382054bb56bc62b8789fa2b68a896c0b267
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-30 11:24:47 -0400

    Initial Users::New action and controller

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,2 +1,4 @@
-# Configure your routes here
-# See: http://www.rubydoc.info/gems/lotus-router/#Usage
+resources :users, only: [:new]
+get "/signup", to: Web::Controllers::Users::New
+
+redirect "/", to: "/signup"
