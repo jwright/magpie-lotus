6a3f3e62b96e744727db010f1275495dc3270cdc
Jamie Wright <jamie@brilliantfantastic.com>
2015-09-30 23:59:01 -0400

    Add sessions#new route, controller action, and template

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,7 @@
 resources :lists, only: :index
 
-resources :sessions, only: [:new, :destroy]
-get "/signin", to: Web::Controllers::Users::New, as: :signin
+resources :sessions, only: [:new, :create, :destroy]
+get "/signin", to: Web::Controllers::Sessions::New, as: :signin
 delete "/signout", to: "sessions#destroy", as: :signout
 
 resources :users, only: [:new, :create, :show]
